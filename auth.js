41258a8a858155315d8ae027fe342ca4acb3b9db
Rashmi Gupta <rashmigupta.official3@gmail.com>
2026-03-30 02:28:23 +0530

    Add files via upload

---
+++ auth.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/User");
+
+// Register (only once)
+router.post("/register", async (req, res) => {
+  const user = new User(req.body);
+  await user.save();
+  res.json({ message: "User registered" });
+});
+
+// Login
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  const user = await User.findOne({ username, password });
+
+  if (!user) {
+    return res.status(400).json({ message: "Invalid credentials" });
+  }
+
+  res.json({ message: "Login successful" });
+});
+
+module.exports = router;
