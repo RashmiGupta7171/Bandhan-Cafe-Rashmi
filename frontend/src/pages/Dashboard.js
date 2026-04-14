02c5af458a4a36d73c77f12b97483057d0c146d9
RashmiGupta7171 <rashmigupta.official3@gmail.com>
2026-04-14 20:16:22 +0530

    removing

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,4 +1,4 @@
-/* eslint-disable react-hooks/exhaustive-deps */
+
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { FaMoneyBill, FaUserCheck, FaChartLine } from "react-icons/fa";
