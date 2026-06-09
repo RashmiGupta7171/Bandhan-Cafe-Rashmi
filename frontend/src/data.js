b51331ca0b4501fc61e8c5e442f0f74f3ee0c52d
RashmiGupta7171 <rashmigupta.official3@gmail.com>
2026-06-09 16:35:12 +0530

    Added files

--- frontend/src/data.js
+++ frontend/src/data.js
@@ -1,8 +1,8 @@
 export const menuData = {
   "Veg Varieties": [
-    { name: "Paneer Pakoda", half: 140, full: 260 },
-    { name: "Paneer Chilli", half: 160, full: 280 },
-    { name: "Paneer Stick", half: 170, full: 300 },
+    { name: "Paneer Pakoda", half: 160, full: 300 },
+    { name: "Paneer Chilli", half: 160, full: 300 },
+    { name: "Paneer Stick", half: 170, full: 320 },
     { name: "Sweet Corn", half: null, full: 90 },
     { name: "Crispy Corn", half: null, full: 130 },
     { name: "French Fries(Masala/Peri Peri)", half: null, full: 110 },
@@ -12,13 +12,13 @@ export const menuData = {
   ],
 
  "Sedeko Items": [
-    { name: "Peanuts Bhuja/Chiura", half: 50, full: 100 },
-    { name: "Bhatmas Bhuja/Chiura", half: 50, full: 100 },
-    { name: "Peanut Bhatmas", half: 80, full: 140 },
-    { name: "Peanut Sadeko", half: 60, full: 100 },
-    { name: "Peanut Chawchaw Sadeko", half: 80, full: 120 },
+    { name: "Peanuts Bhuja/Chiura", half: 60, full: 120 },
+    { name: "Bhatmas Bhuja/Chiura", half: 60, full: 120 },
+    { name: "Peanut Bhatmas", half: 80, full: 150 },
+    { name: "Peanut Sadeko", half: 60, full: 120 },
+    { name: "Peanut Chawchaw Sadeko", half: 100, full: 190 },
     { name: "Chawchaw Sadeko", half: null, full: 60 },
-     { name: "ChawChaw Chiura/Bhuja Sadeko", half: null, full: 70 },
+     { name: "ChawChaw Chiura/Bhuja Sadeko", half: null, full: 90 },
       { name: "Kaju Fry", half: 150, full: 250 },
       { name: "Prawn Chips", half: 50, full: 100 },
       { name: "Pop Corn", half: null, full: 30 },
@@ -41,7 +41,7 @@ export const menuData = {
   "Momo Items": [
     { name: "Veg Momo", half: 60, full: 120 },
     { name: "Fry Veg Momo", half: 70, full: 130 },
-    { name: "Veg Jhol Momo", half: null, full: 150 },
+    { name: "Veg Jhol Momo", half: 80, full: 150 },
     { name: "Veg Chilli Momo", half: 100, full: 190 },
     { name: "Chicken Momo", half: 80, full: 150 },
     { name: "Chicken Fry Momo", half: 90, full: 160 },
@@ -50,13 +50,13 @@ export const menuData = {
   ],
 
    "Chicken Items": [
-    { name: "Chicken Fry", half: 140, full: 260 },
-    { name: "Chicken Sekuwa", half: 150, full: 280 },
+    { name: "Chicken Fry", half: 150, full: 290 },
+    { name: "Chicken Sekuwa", half: 150, full: 300 },
     { name: "Chicken Lollipop Fry", half: 160, full: 300 },
     { name: "Chicken Chilly", half: 170, full: 350 },
-    { name: "Chicken Leg Fry", half: null, full: 200 },
+    { name: "Chicken Leg Fry", half: 160, full: 220 },
     { name: "Chicken leg Sekuwa", half: null, full: 240 },
-   { name: "Chicken Gravy", half: 110, full: 200 },
+   { name: "Chicken Gravy", half: 130, full: 250 },
     { name: "Chicken Manchurian(Dry)", half: 180, full: 340 },
      { name: "Chicken Manchurian(Gravy)", half: 200, full: 360 },
      { name: "Chicken Sausage(6pc)", half: 150, full: 300 },
@@ -65,7 +65,7 @@ export const menuData = {
 
 
  "Mutton Items": [
-    { name: "Mutton Gravy", half: 180, full: 350 },
+    { name: "Mutton Gravy", half: 200, full: 390 },
     { name: "Mutton Sekuwa", half: 190, full: 380 },
     { name: "Mutton Bhutuwa", half: 120, full: 230 },
     { name: "Mutton Chapli Kabab", half: null, full: 300 }
@@ -73,7 +73,9 @@ export const menuData = {
 
   "Egg Items": [
     { name: "Boiled Egg", half: 80, full: 150 },
-    { name: "Omlette", half: 40, full: 70 },
+    { name: "Omlette", half: 40, full: 80 },
+    { name: "Egg Chauchau", half: null, full: 100 },
+    { name: "Egg Current", half: null, full: 110 },
   ],
 
 
@@ -82,7 +84,7 @@ export const menuData = {
     { name: "Plain Rice", half: null, full: 60 },
     { name: "Jeera Rice", half: null, full: 70 },
     { name: "Veg Fried Rice", half: null, full: 90 },
-    { name: "Paneer Fried Rice", half: null, full: 110 },
+    { name: "Paneer Fried Rice", half: null, full: 140 },
     { name: "Egg Fried Rice", half: null, full: 100 },
     { name: "Chicken Fried Rice", half: null, full: 140 },
      { name: "Egg Chicken Fried Rice", half: null, full: 160 }
@@ -95,8 +97,35 @@ export const menuData = {
   ],
 
   "Beverages": [
-    { name: "Tea", half: null, full: 25 },
-    { name: "Coffee", half: null, full: 30 },
+    { name: "Tea", half: null, full: 30 },
+    { name: "Coffee", half: null, full: 40 },
     { name: "Hookah", half: null, full: 450 },
+    { name: "Coke/Sprite", half: null, full: 60 },
+    { name: "Frooti", half: null, full: 30 },
+    { name: "Hookah Premium", half: null, full: 500 },
+    { name: "RedBull", half: null, full: 120 },
+    { name: "water", half: null, full: 30 },
+    { name: "Xtreme", half: null, full: 150 },
   ]
+ "Alcohol": [
+    { name: "Small Arna Beer", half: null, full: 220 },
+    { name: "Big Arna Beer", half: null, full: 450 },
+    { name: "Bara Singha(Beer)", half: null, full: 220 },
+    { name: "Bara Singh(Beer)", half: null, full: 430 },
+    { name: "Big Master", half: null, full: 1100 },
+    { name: "Black Oak", half: null, full: 400 },
+    { name: "Blue Diamond", half: null, full: 350 },
+    { name: "Golden Oak", half: null, full: 350 },
+    { name: "Gorkha Beer", half: null, full: 230 },
+    { name: "Gurkhas & Guns", half: null, full: 1900 },
+    { name: "Kala Patthar", half: null, full: 800 },
+    { name: "Kala Patthar", half: null, full: 400 },
+    { name: "Old Durbar", half: null, full: 800 },
+    { name: "Signature Red", half: null, full: 750 },
+    { name: "Tuborg Can ", half: null, full: 400 },
+    { name: "Tuborg Gold", half: null, full: 550 },
+    { name: "Tuborg Strong", half: null, full: 220 },
+    { name: "Vodka(180ml)", half: null, full: 650 },
+  ]
+
 };
